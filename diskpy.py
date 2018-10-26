9f8a1d44557c40b7b0d7631171b97a0eb36268fc
Daniel Bodarev <daniel.bodarev@jessup.edu>
2018-10-26 13:43:12 -0700

    disk_init done

--- diskpy.py
+++ diskpy.py
@@ -1,19 +1,21 @@
-DISK_BLOCK_SIZE = 512  
+DISK_BLOCK_SIZE = 512 
 
-def disk_init(filename, nblocks = 100):
-    pass
+def disk_init(filename, nblocks=100):
+    with open(filename, 'wb+') as f:
+        test = [0 for i in range(nblocks * DISK_BLOCK_SIZE)]
+        f.write(bytearray(test))
     
 def disk_open(filename):
+    pass
+    
+def disk_size(filename):
     pass 
     
-def disk_size():
-    pass 
-    
-def disk_read( blocknum ):
+def disk_read(filename, blocknum):
     pass
 
-def disk_write( blocknum, data ):
+def disk_write(filename, blocknum, data):
     pass 
     
-def disk_close():
+def disk_close(filename):
     pass
