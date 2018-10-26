931e724ddaa2a30039ae71e50d42b585045dceb5
Daniel Bodarev <daniel.bodarev@jessup.edu>
2018-10-26 12:59:35 -0700

    Initialization

---
+++ diskpy.py
@@ -0,0 +1,19 @@
+DISK_BLOCK_SIZE = 512  
+
+def disk_init(filename, nblocks = 100 ):
+    pass
+    
+def disk_open(filename):
+    pass 
+    
+def disk_size():
+    pass 
+    
+def disk_read( blocknum ):
+    pass
+
+def disk_write( blocknum, data ):
+    pass 
+    
+def disk_close():
+    pass
