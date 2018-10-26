5be1a540f710cd7c40c3c7bae9402f310ae30170
Daniel Bodarev <daniel.bodarev@jessup.edu>
2018-10-26 14:28:38 -0700

    halfway through disk_write, rewrote disk_init

--- diskpy.py
+++ diskpy.py
@@ -1,9 +1,11 @@
+import os
+
 DISK_BLOCK_SIZE = 512 
 
 def disk_init(filename, nblocks=100):
     with open(filename, 'wb+') as f:
-        test = [0 for i in range(nblocks * DISK_BLOCK_SIZE)]
-        f.write(bytearray(test))
+        blocks = [0 for i in range(DISK_BLOCK_SIZE)]
+        [f.write(blocks) for i in range(nblocks)]
     
 def disk_open(filename):
     try:
@@ -18,7 +20,14 @@ def disk_read(filename, blocknum):
     pass
 
 def disk_write(filename, blocknum, data):
-    pass
+
+    old_file = "OLD_"+filename
+
+    assert(len(data) <= DISK_BLOCK_SIZE), "INPUT DATA TOO LARGE; MAXIMUM {} BYTES; {} GIVEN".format(DISK_BLOCK_SIZE, len(data))
+
+    with open(filename, "wb+") as new, open(old_file, "rb") as old:
+        for line in old:
+            new.write(line)
     
     
 def disk_close(stream):
@@ -26,3 +35,5 @@ def disk_close(stream):
         stream.close()
     except:
         print("COULD NOT CLOSE STREAM")
+
+disk_write("hello.123", 0, [22 for i in range(DISK_BLOCK_SIZE)])
